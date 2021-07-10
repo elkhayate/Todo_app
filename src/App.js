0a86456cf056f953179a154671942d22df249ad3
elkhayate <melkhayate61@gmail.com>
2021-07-11 00:34:49 +0300

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import style from "./app.module.css";
+import todo from "./img/todo.svg";
 
 
 function App() {
   return (
-    <div>
-
+    <div className={style.container}>
+      <div className={style.title}>
+        <h1>Todo App w/ Hooks.</h1>
+      </div>
+          <div className={style.data}>
+              <div className={style.svg}> 
+                <img src={todo} alt="todo svg" />
+              </div>
+          </div>
+          <button className={style.btn}>Get Started</button>
     </div>
   );
 }
---
+++ src/components/TodoApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import style from "../styles/todoapp.module.css";
+
+
+function TodoApp() {
+    return(
+        <div className={style.container}>
+            
+        </div>
+    )
+}
