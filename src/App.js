657e4a0e276fb7acc699fdede727193a78c0b17a
elkhayate <melkhayate61@gmail.com>
2021-07-11 02:41:03 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import style from "./app.module.css";
 import todo from "./img/todo.svg";
-
-
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import TodoApp from "./components/TodoApp";
 function App() {
   return (
+    
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+        <Route  path="/Todo" component={TodoApp}/>
+      </Switch>
+    
+    
+    </Router>
+    
+  );
+}
+
+export default App;
+
+function Landing() {
+  const Style = {textDecoration: 'none', color : "white"};
+  return(
+    <>
     <div className={style.container}>
       <div className={style.title}>
         <h1>Todo App w/ Hooks.</h1>
@@ -14,9 +33,8 @@ function App() {
                 <img src={todo} alt="todo svg" />
               </div>
           </div>
-          <button className={style.btn}>Get Started</button>
+          <button className={style.btn}><Link to="/Todo" style={Style}>Get Started</Link></button>
     </div>
-  );
+    </>
+  )
 }
-
-export default App;
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -5,7 +5,9 @@ import style from "../styles/todoapp.module.css";
 function TodoApp() {
     return(
         <div className={style.container}>
-            
+            <h1>simo</h1>
         </div>
     )
 }
+
+export default TodoApp;
