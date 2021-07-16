440f24b941bd8fecfbfbb9849e0d71706b58f399
elkhayate <melkhayate61@gmail.com>
2021-07-16 12:01:33 +0300

    befor media

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import style from "../styles/item.module.css";
-import edit from "../img/edit.png";
 import delet from "../img/delete.png"
 
 export default function TodoItem(props) {
@@ -8,7 +7,6 @@ export default function TodoItem(props) {
         <div className={style.container}>
             <h1>{props.task}</h1>
             <div className={style.btns}>
-            <button className={style.btn}><img src={edit} alt = "edit"/></button>
             <button onClick={props.delete} className={style.btn}><img src={delet} alt = "delete"/></button>
             </div>
         </div>
